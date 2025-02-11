f7697cafd2bb63b900781932c3270c9f737a724a
Tru Lie <84538219+TruLie13@users.noreply.github.com>
2025-02-11 09:49:34 -0700

    footer text on one line - responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,16 +7,22 @@ export default function Footer() {
     <div className="w-full h-20 bg-black flex items-center justify-center relative">
       <Box className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-gray-900 via-black to-gray-900 opacity-70 z-10"></Box>
 
-      <div className="text-center z-20">
-        {/* Link the text */}
-        <Typography variant="h6" className="text-white font-light">
+      <div className="text-center z-20 text-white font-light flex justify-center items-center flex-wrap gap-2 text-sm sm:text-base md:text-lg">
+        <Typography component="span">
           <Link
             href="https://pi-que.com"
-            className="text-white hover:underline"
+            className="text-white group"
             target="_blank"
           >
-            Created by pi.que © 2025
+            Created by{" "}
+            <span className="text-white group-hover:text-red-500 transition-colors duration-300">
+              pi.que llc
+            </span>{" "}
+            © 2025.
           </Link>
+        </Typography>
+        <Typography component="span" className="whitespace-nowrap">
+          All rights reserved
         </Typography>
       </div>
 
