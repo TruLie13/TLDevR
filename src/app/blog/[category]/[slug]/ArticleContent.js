529e195f5ccafd3539578a3b33e5aedc1a0e14a1
Tru Lie <84538219+TruLie13@users.noreply.github.com>
2025-02-19 15:07:04 -0700

    implemented semantic HTML on article content

--- src/app/blog/[category]/[slug]/ArticleContent.js
+++ src/app/blog/[category]/[slug]/ArticleContent.js
@@ -80,6 +80,14 @@ export default function ArticleContent({ article }) {
   `;
 
   const validImageUrl = imageSrc || getValidImageUrl(article.image);
+  const formattedDate = new Date(article.publishedAt).toLocaleDateString(
+    "en-US",
+    {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }
+  );
 
   return (
     <div>
@@ -96,127 +104,96 @@ export default function ArticleContent({ article }) {
           padding: "1rem",
         }}
       >
-        {/* Image Card */}
-        <Box sx={{ position: "relative", width: "100%" }}>
-          <Card
-            sx={{
-              width: "100%",
-              height: { xs: "35vh", sm: "35vh" },
-              borderRadius: "1.5rem",
-              overflow: "hidden",
-              backgroundColor: "rgb(8, 4, 31)",
-            }}
-          >
-            {validImageUrl && (
-              <MemoizedImage
-                src={validImageUrl}
-                alt={`Image for article: ${article.title}`}
-                onError={handleImageError}
-                sizes="(max-width: 600px) 100vw, 50vw"
-              />
-            )}
-          </Card>
-
-          {/* Floating Action Buttons */}
-          <Box
-            sx={{
-              position: "absolute",
-              bottom: "-25px",
-              right: "5%",
-              display: "flex",
-              gap: "10px",
-              zIndex: 10,
-            }}
-          >
-            <IconButton
+        <article style={{ width: "100%" }}>
+          <Box sx={{ position: "relative", width: "100%" }}>
+            <Card
               sx={{
-                backgroundColor: "rgba(0,0,0,0.7)",
-                color: "white",
-                width: "50px",
-                height: "50px",
-                borderRadius: "50%",
-                "&:hover": { backgroundColor: "rgba(0,0,0,0.9)" },
-              }}
-              onClick={handleShareClick}
-            >
-              <Share />
-            </IconButton>
-
-            <IconButton
-              sx={{
-                backgroundColor: "rgba(0,0,0,0.7)",
-                color: "white",
-                width: "50px",
-                height: "50px",
-                borderRadius: "50%",
-                "&:hover": { backgroundColor: "rgba(0,0,0,0.9)" },
+                width: "100%",
+                height: { xs: "35vh", sm: "35vh" },
+                borderRadius: "1.5rem",
+                overflow: "hidden",
+                backgroundColor: "rgb(8, 4, 31)",
               }}
             >
-              <Favorite />
-            </IconButton>
+              {validImageUrl && (
+                <MemoizedImage
+                  src={validImageUrl}
+                  alt={`Image for article: ${article.title}`}
+                  onError={handleImageError}
+                  sizes="(max-width: 600px) 100vw, 50vw"
+                />
+              )}
+            </Card>
+            <Box
+              sx={{
+                position: "absolute",
+                bottom: "-25px",
+                right: "5%",
+                display: "flex",
+                gap: "10px",
+                zIndex: 10,
+              }}
+            >
+              <IconButton
+                sx={{
+                  backgroundColor: "rgba(0,0,0,0.7)",
+                  color: "white",
+                  width: "50px",
+                  height: "50px",
+                  borderRadius: "50%",
+                  "&:hover": { backgroundColor: "rgba(0,0,0,0.9)" },
+                }}
+                onClick={handleShareClick}
+              >
+                <Share />
+              </IconButton>
+
+              <IconButton
+                sx={{
+                  backgroundColor: "rgba(0,0,0,0.7)",
+                  color: "white",
+                  width: "50px",
+                  height: "50px",
+                  borderRadius: "50%",
+                  "&:hover": { backgroundColor: "rgba(0,0,0,0.9)" },
+                }}
+              >
+                <Favorite />
+              </IconButton>
+            </Box>
           </Box>
-        </Box>
 
-        {/* Article Content */}
-        <CardContent
-          sx={{ textAlign: "left", width: "100%", mt: 4, marginTop: "1rem" }}
-        >
-          <Typography
-            variant="h4"
-            fontWeight="bold"
-            gutterBottom
-            sx={{ color: "white" }}
-          >
-            {article.title}
-          </Typography>
+          <CardContent sx={{ textAlign: "left", width: "100%", mt: 4 }}>
+            <header>
+              <Typography
+                variant="h4"
+                fontWeight="bold"
+                gutterBottom
+                sx={{ color: "white" }}
+              >
+                {article.title}
+              </Typography>
+              <Typography
+                variant="body2"
+                sx={{ color: "rgba(255, 255, 255, 0.6)" }}
+              >
+                Published on {formattedDate}
+              </Typography>
+            </header>
 
-          <Box
-            className="tiptap-content"
-            sx={{
-              color: "rgba(255,255,255,0.8)",
-              textAlign: "justify",
-              width: "100%",
-              "& a": {
-                color: "#3498db",
-                textDecoration: "underline",
-                "&:hover": {
-                  color: "#2980b9",
-                },
-              },
-              "& p": { marginBottom: "16px" },
-              "& strong": { fontWeight: "bold" },
-              "& em": { fontStyle: "italic" },
-              "& u": { textDecoration: "underline" },
-              // Styling for number lists
-              "& ol": {
-                listStyleType: "decimal", // Display ordered (numbered) list
-                marginLeft: "20px", // Add space on the left
-                paddingLeft: "10px", // Optional padding for spacing
-              },
-              "& li": {
-                marginBottom: "8px", // Space between list items
-              },
-              // Styling for bullet lists
-              "& ul": {
-                listStyleType: "disc", // Display unordered (bullet) list
-                marginLeft: "20px", // Add space on the left
-                paddingLeft: "10px", // Optional padding for spacing
-              },
-              // Styling for quote blocks
-              "& blockquote": {
-                borderLeft: "4px solid #3498db", // Blue left border for quotes
-                paddingLeft: "16px", // Space between border and text
-                fontStyle: "italic", // Italic text for blockquotes
-                margin: "16px 0", // Space above and below the quote block
-                color: "rgba(255, 255, 255, 0.8)", // Optional: same color as text
-              },
-            }}
-          >
-            {highlightedContent}
-          </Box>
-        </CardContent>
+            <Box
+              className="tiptap-content"
+              sx={{
+                color: "rgba(255,255,255,0.8)",
+                textAlign: "justify",
+                width: "100%",
+              }}
+            >
+              {highlightedContent}
+            </Box>
+          </CardContent>
+        </article>
 
-        {/* Snackbar for Toast message */}
         <SnackbarComponent
           open={openSnackbar}
           onClose={handleCloseSnackbar}
