a3dc5c139d80dd87f8db20c41282935673a0401f
Tru Lie <84538219+TruLie13@users.noreply.github.com>
2025-02-19 20:34:56 -0700

    resolved create card maargin issu

--- src/components/TipTap/TipTap.js
+++ src/components/TipTap/TipTap.js
@@ -104,7 +104,7 @@ const Tiptap = ({ onChange }) => {
 
   return (
     <Card
-      className="p-5"
+      className="p-5 mt-10"
       sx={{
         backgroundColor: "rgb(21, 18, 43)",
         width: "100%",
