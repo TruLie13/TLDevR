67e137ce587ae81d0589e59095a9fbc403bef0d5
Tru Lie <84538219+TruLie13@users.noreply.github.com>
2025-03-01 18:00:11 -0700

    switch to live backend url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,5 @@
-const apiUrl = process.env.NEXT_PUBLIC_API_URL + "/api";
+// const apiUrl = process.env.NEXT_PUBLIC_API_URL + "/api";
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 const isWindowUndefined = typeof window === "undefined";
 
