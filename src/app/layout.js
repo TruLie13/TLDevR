c6314ac0e1619a87cf42dbaac2ac5aa29546116b
Tru Lie <84538219+TruLie13@users.noreply.github.com>
2025-03-03 17:34:16 -0700

    add Google Adsense script to head

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "../../dist/output.css";
 import "./globals.css";
 import QueryProvider from "./QueryProvider";
+import Head from "next/head.js";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -18,6 +19,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.variable}>
+      <Head>
+        {/* Google AdSense script */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1337305196700921"
+          crossorigin="anonymous"
+        ></script>
+      </Head>
       <body className="min-h-screen flex flex-col">
         <Navbar />
         <main className="flex-grow pt-16">
